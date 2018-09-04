664d665fdad427b63d985ed4b7bd7e519c54f4fe
juliansirkin <juliansirkin@gmail.com>
2018-09-04 09:30:43 -0400

    Update production API location

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/blooming-spire-83563.git',
+  production: 'https://workoutapi.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
